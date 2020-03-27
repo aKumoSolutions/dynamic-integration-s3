fabca5a067059ce53c2fdf77e7fa7025df7020a6
khuslens <khuslen.cyber@gmail.com>
2020-03-27 11:00:19 -0500

    { is added

--- api-gw.tf
+++ api-gw.tf
@@ -3,7 +3,7 @@ resource "aws_api_gateway_rest_api" "mail_api" {
   description = "This API is for trigger lamnda"
 }
 
-resource "aws_api_gateway_resource" "mail_api_resource" 
+resource "aws_api_gateway_resource" "mail_api_resource" {
   rest_api_id = aws_api_gateway_rest_api.mail_api.id
   parent_id   = aws_api_gateway_rest_api.mail_api.root_resource_id
   path_part   = "index.html"
